246d2e44362a96f44d4c0586066fb14405295832
nc1515 <nuno.caldeira-pereira15@imperial.ac.uk>
2019-03-14 14:46:23 +0000

    Pose to target object. Tested gaze integration. Tested simple object avoidance with the real robot

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,11 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/faisallab008/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export CPATH="/home/faisallab008/catkin_ws/devel/include:$CPATH"
-export LD_LIBRARY_PATH="/home/faisallab008/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/faisallab008/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/faisallab008/catkin_ws/build"
-export PYTHONPATH="/home/faisallab008/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/faisallab008/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/faisallab008/catkin_ws/src:$ROS_PACKAGE_PATH"
