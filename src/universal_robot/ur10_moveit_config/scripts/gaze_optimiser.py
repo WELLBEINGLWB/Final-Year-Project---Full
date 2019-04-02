e1929d7547131a9d1cab0124343c8cfae9090d1a
nc1515 <nuno.caldeira-pereira15@imperial.ac.uk>
2019-04-02 12:04:52 +0100

    Added loop to create occupancy grid, object checking for all grasp points and find trajectory with Astar

--- src/universal_robot/ur10_moveit_config/scripts/gaze_optimiser.py
+++ src/universal_robot/ur10_moveit_config/scripts/gaze_optimiser.py
@@ -17,11 +17,113 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.patches as patches
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from matplotlib import colors
+
+
+
 
 def gaze_optimiser_server():
     s = rospy.Service('gaze_optimiser_service', gazeOptimiser, handle_objects)
     print "Ready to receive objects."
     rospy.spin()
+
+
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def astar(maze, start, end):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
 
 def knn_search(x, D, K):
      """ find nearest neighbours of gaze point """
@@ -121,13 +223,13 @@ def ik_calculator(grasp_point):
 def line_collision(x1,y1,x2,y2,x3,y3,x4,y4):
     uA = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1))
     uB = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1))
-    print(uA)
-    print(uB)
+    # print(uA)
+    # print(uB)
     if( 0<= uA <= 1 and 0<= uB <= 1):
         intersectionX = x1 + (uA*(x2-x1))
         intersectionY = y1 + (uB*(y2-y1))
-        print(intersectionX)
-        print(intersectionY)
+        # print(intersectionX)
+        # print(intersectionY)
         return True
     return False
 
@@ -361,6 +463,7 @@ def sim(req):
 
 
     fig,ax = plt.subplots(1)
+#    plt.figure(figsize=(10,10/(1.7/0.7)))
     # fig3 = plt.figure(2)
     # ax3 = fig3.gca(projection='3d')
     # fig.set_size_inches(18.5, 10.5)
@@ -381,8 +484,9 @@ def sim(req):
         j+=6
 
     print(center)
-
-    x = np.array([[.65],[0.8]])
+    ##########~~~~~~~~~~~~~~~~~~~~~
+    # Gaze point xy
+    x = np.array([[.39],[0.84]])
     neig_idx = knn_search(x,center,1)
     print("Index %s" %neig_idx)
     print("target object xyz:")
@@ -399,15 +503,62 @@ def sim(req):
     objects_array = world_objects.data
     # co_e, co_s = ik_calculator(grasp_point)
     co_e, co_s = angle_ik(grasp_point)
-    print("Elbow point: %s" %co_e)
-    print("Grasp point: %s" %grasp_point)
+    #print("Elbow point: %s" %co_e)
+    #print("Grasp point: %s" %grasp_point)
     # rect = patches.Rectangle((0.5,0.2),0.15,0.1,linewidth=1,edgecolor='r',facecolor='none')
     # ax.add_patch(rect)
     collision = object_collision(co_e,grasp_point, objects_array)
-    print("Bot: %s" %bot)
+    #~~~~~~~~~~~~~~~~~~~~
 
-    # highlighting the neighbours
-    pylab.plot(center[1,neig_idx],center[0,neig_idx],'o', markerfacecolor='None',markersize=15,markeredgewidth=1)
+    ratio = 1.7/0.7
+    rows = 80
+    cols = int(rows/ratio)
+    data = [[0 for _ in range(rows)] for _ in range(cols)]
+    data[20][26]=1
+    data[20][25]=1
+    data[19][28]=1
+    Yresolution = 1.7/rows
+    Xresolution = 0.7/cols
+    # target=[0.22,0.534]
+    # target_index = [round(target[0]/Xresolution),round(target[1]/Yresolution)]
+    target_x = world_objects.data[neig_idx*6 + 3] - world_objects.data[neig_idx*6]/2 - offset_x
+    target_y = world_objects.data[neig_idx*6 + 4] - world_objects.data[neig_idx*6 + 1]/2 - offset_y
+    target_index = [round(target_x/Xresolution)-1,round(target_y/Yresolution)-1]
+    start = (8, 20)
+    end = (int(target_index[0]), int(target_index[1]))
+    for i in range(rows):
+        for j in range(cols):
+            #target = [Xresolution*(j+1),Yresolution*(i+1)]
+            grasp_point.x = Xresolution*(j+1)
+            grasp_point.y = Yresolution*(i+1)
+            co_e, co_s = angle_ik(grasp_point)
+            collision_state = object_collision(co_e,grasp_point, objects_array)
+            if(collision_state == True):
+                data[j][i] = 1
+
+    if data[end[0]][end[1]] != 1:
+        path = astar(data, start, end)
+        print(path)
+        for j in range(len(path)):
+                r = path[j][0]
+                c = path[j][1]
+#                print(r,c)
+                data[r][c]= 4
+    data[end[0]][end[1]]=2
+    data[start[0]][start[1]]=3
+    data = np.array(data)
+    data.shape
+    fig2 = plt.figure(figsize=(10,10/ratio))
+    cmap = colors.ListedColormap(['white','red','green','#0060ff','#aaaaaa'])
+    plt.figure(figsize=(10,10/ratio))
+    plt.pcolor(data[::1],cmap=cmap,edgecolors='k', linewidths=1)
+    fig2.gca().invert_xaxis()
+    fig2.show()
+    # rospy.sleep(10.0)
+    # plt.close('all')
+
+
+
 
 
     # plotting the data and the input point
@@ -415,13 +566,19 @@ def sim(req):
     # ax3.scatter([0], [0], [0], color="g", s=100)
     # plt.plot([co_s.y,co_e.y,grasp_point.y],[co_s.x,co_e.x,grasp_point.x],[co_s.z,co_e.z,grasp_point.z])
 
-    plt.axis([-0.2,1.7, -0.2, 0.75])
+    ax.axis([-0.2,1.7, -0.2, 0.75])
     # ax.axis([-0.2,1.7, -0.2, 0.75])
     # Plot center of objects, grasp point and elbow joint
+    # highlighting the target object
+    ax.plot(center[1,neig_idx],center[0,neig_idx],'o', markerfacecolor='None',markersize=15,markeredgewidth=1)
+    grasp_point.x = world_objects.data[neig_idx*6 + 3] - world_objects.data[neig_idx*6]/2 - offset_x
+    grasp_point.y = world_objects.data[neig_idx*6 + 4] - world_objects.data[neig_idx*6 + 1]/2 - offset_y
+    co_e, co_s = angle_ik(grasp_point)
     ax.plot(center[1,:],center[0,:],'ob',x[1,0],x[0,0],'or',grasp_point.y,grasp_point.x,'og',co_e.y,co_e.x,'ok')
     # ax.plot(0.525,0.380,'oy')
     # Mark target object
     ax.plot(center[1,neig_idx],center[0,neig_idx],'o', markerfacecolor='None',markersize=15,markeredgewidth=1)
+
     ax.plot([co_s.y,co_e.y,grasp_point.y],[co_s.x,co_e.x,grasp_point.x])
     plt.gca().invert_xaxis()
 
@@ -459,7 +616,7 @@ if __name__ == '__main__':
 
     # gaze_optimiser_server()
     req = [0.08420228958129883, 0.0866108238697052, 0.16340254247188568, 0.5440202951431274, 0.07170078903436661, 0.11486805230379105, 0.044422149658203125, 0.054751671850681305, 0.11145603656768799, 0.4410666823387146, -0.08078508079051971, 0.02292603999376297, 0.03234878182411194, 0.03743894398212433, 0.046912916004657745, 0.45620059967041016, 0.2094809114933014, 0.030203916132450104]
-    req = [0.10357585549354553, 0.07956766337156296, 0.1900981366634369, 0.3480975031852722, 0.038999784737825394, 0.025573041290044785, 0.09558302164077759, 0.06678294390439987, 0.1394098997116089, 0.3654365837574005, -0.11884936690330505, -0.04283341020345688, 0.02408367395401001, 0.07848946005105972, 0.06253930926322937, 0.5783331394195557, -0.037476059049367905, 0.04785224795341492]
+    # req = [0.10357585549354553, 0.07956766337156296, 0.1900981366634369, 0.3480975031852722, 0.038999784737825394, 0.025573041290044785, 0.09558302164077759, 0.06678294390439987, 0.1394098997116089, 0.3654365837574005, -0.11884936690330505, -0.04283341020345688, 0.02408367395401001, 0.07848946005105972, 0.06253930926322937, 0.5783331394195557, -0.037476059049367905, 0.04785224795341492]
     sim(req)
     # handle_objects(req)
     rospy.spin()
